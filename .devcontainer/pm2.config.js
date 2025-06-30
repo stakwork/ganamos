9ad375714db55b6c9610ff2c06b6a7672e9bde79
Gonzalo Javier Aune <gonzaloaune@gmail.com>
2025-06-30 14:31:43 +0100

    added devcontainer

---
+++ .devcontainer/pm2.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: "frontend",
+      script: "npm start",
+      cwd: "/workspaces/ganamos",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        PORT: "3000"
+      }
+    }
+  ],
+};
+
+// "GROQ_API_KEY":"",
+// "X-CMC_PRO_API_KEY":"",
+// "NODE_ENV":"",
+// "LND_REST_URL":"",
+// "LND_ADMIN_MACAROON":"",
+// "NEXT_PUBLIC_SUPABASE_URL": "",
+// "NEXT_PUBLIC_SUPABASE_ANON_KEY": "",
+// "SUPABASE_SERVICE_ROLE_KEY": "",
+// "NEXT_PUBLIC_GOOGLE_MAPS_API_KEY": "",
+// "GOOGLE_MAPS_API_KEY": ""
